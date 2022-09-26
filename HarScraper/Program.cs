061289779eb42c0e5a87af74024a78629e853e45
Quizmo <quizmo22@gmail.com>
2022-09-26 12:38:12 +0200

    Code

---
+++ HarScraper/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json.Nodes;
+
+string fileName = "Input/input.har";
+using FileStream openStream = File.OpenRead(fileName);
+var entries = ((dynamic)JsonNode.Parse(openStream))["log"]["entries"];
+var total = entries.Count;
+var i = 0;
+foreach (var entry in entries)
+{
+    i++;
+    var url = (string)entry["request"]["url"];
+    var filePath = url.Replace(new Uri(url).GetLeftPart(UriPartial.Authority), string.Empty);
+    var path = Path.GetDirectoryName(filePath);
+    if (!Directory.Exists($"Output/{path}"))
+    {
+        Directory.CreateDirectory($"Output/{path}");
+    }
+
+    var content = await DownloadFile(url);
+    if (content == null)
+    {
+        Console.WriteLine("Failed to download" + url);
+        continue;
+    }
+    await File.WriteAllBytesAsync($"Output/{path}/{Path.GetFileName(filePath).Split("?")[0]}", content);
+    Console.WriteLine($"File downloaded {i} out of {total}");
+}
+
+static async Task<byte[]?> DownloadFile(string url)
+{
+    using (var client = new HttpClient())
+    using (var result = await client.GetAsync(url))
+        return result.IsSuccessStatusCode ? await result.Content.ReadAsByteArrayAsync() : null;
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # HarScraper
 Scrapes and download the content from har files
+
+To use download the har file from the browsers network tab, rename it to input.har and place it under bin\Release\net6.0\Input
+
+Then run the HarScraper.exe under bin\Release\net6.0.
+
+All content should be downloaded and placed in the folder called Output.
+
+This has only been tested with Firefox, har files from Chrome might be different and might not work as expected.
